641b10f7ebf17707c29f5064da0725cc2baf5e8c
lukasklier-v-sion <lukas.klier@v-sion.de>
2026-01-21 14:58:45 +0100

    fix: align layout in title block

--- app/components/content/TitleBlock.vue
+++ app/components/content/TitleBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="hero">
     <div id="headline">
-      <div class="headline-content wrapper">
+      <div class="headline-content">
         <h1 :class="{ 'text-right': textPos === 'r' }">
           <slot v-if="$slots.default" mdc-unwrap="p" />
         </h1>
@@ -69,8 +69,8 @@ defineProps<{
 #headline {
   position: relative;
   max-width: 100%;
-  padding: clamp(10rem, 12vw, 13.75rem) clamp(2rem, 10vw, 19.125rem)
-    clamp(2rem, 5vw, 4rem) 0;
+  padding: clamp(10rem, 12vw, 13.75rem) clamp(1rem, 10vw, 19.125rem)
+    clamp(2rem, 5vw, 4rem) clamp(1rem, 5vw, 5.625rem);
   overflow-x: clip;
 }
 
