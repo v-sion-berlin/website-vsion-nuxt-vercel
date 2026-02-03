8c96db23380cb75d911eb62ef3c2eba4e306b6e4
lukasklier-v-sion <lukas.klier@v-sion.de>
2026-02-03 09:58:28 +0100

    refactor: remove Google Fonts preconnect and stylesheet configuration

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
 import "./styles/global.css";
-import { useHead } from "#imports";
-
-useHead({
-  link: [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;700;800&display=swap",
-    },
-  ],
-});
 </script>
 
 <template>
