cb632c9ed4f8d92d88d1b1d68f94c7edad6b043d
lukasklier-v-sion <lukas.klier@v-sion.de>
2026-02-26 16:32:57 +0100

    feat: add correct lang watcher

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -36,7 +36,7 @@ const { data: projects } = await useAsyncData(
 );
 
 const { data: contactDataRaw } = await useAsyncData(
-  `contact-data`,
+  `contact-data` + locale,
   () =>
     queryCollection(
       withoutTrailingSlash(`contact_${locale.value}`) as keyof Collections,
